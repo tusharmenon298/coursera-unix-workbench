260d9bb56c1c9aa55b64409c5eda57e9114074c0
tusharmenon298 <tushar.kumar298@gmail.com>
2020-04-25 17:05:33 +0530

    Improved guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,25 +1,31 @@
 #!/usr/bin/env bash
-# File: math.sh
+# File: guessinggame.sh
 
 function checkfiles {
-filecounter=$(pwd | ls| wc -l)
+filecounter=$( ls -a| wc -l)
 flag=1
 echo "Hello User, guess the number of file in current directory "
 while [[ $flag -eq 1 ]]
 do
 	read response
-	if [[ $response -gt $filecounter ]]
+	
+	if [[ $response =~ ^[0-9]+$ ]]
 	then
-		echo "You guessed too high. Guess again and go lower this time"
-	elif [[ $response -eq $filecounter ]]
-	then
-		let flag=0
+		if [[ $response -gt $filecounter ]]
+		then
+			echo "You guessed too high. Guess again and go lower this time"
+		elif [[ $response -eq $filecounter ]]
+		then
+			let flag=0
+		else
+			echo "You guessed too low. Guess again and go higher this time"
+		fi
 	else
-		echo "You guessed too low. Guess again and go higher this time"
+		echo "Please enter a number"
 	fi
 done
 echo "Congrats, That is the right answer"
 }
 
 checkfiles
-echo Script ended 
+echo "Script ended" 
